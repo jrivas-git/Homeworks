c43ae962a66618ad1dc336a1f7c197320b15fd1d
jrivas <juan_pablo.rivas@uao.edu.co>
2026-03-24 11:49:36 -0500

    subir actividad Context Api

---
+++ ContextAct/src/Hooks/UseAuth.tsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+export function UseAuth() {
+  const [user, setUser] = useState<any>(null);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  const login = (email: string, password: string) => {
+    if (email === "user@mail.com" && password === "123") {
+      const userData = { email };
+      setUser(userData);
+      localStorage.setItem("user", JSON.stringify(userData));
+      return true;
+    }
+    return false;
+  };
+
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem("user");
+  };
+
+  return { user, login, logout };
+}
---
+++ ContextAct/src/Pages/Page1.tsx
@@ -0,0 +1,3 @@
+export default function Page1() {
+  return <h1>Exercise 1</h1>;
+}

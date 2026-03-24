58e8e8e479fdec471070247ab62be1ea1228157d
jrivas <juan_pablo.rivas@uao.edu.co>
2026-03-24 11:44:27 -0500

    subir actividad Context Api

--- ContextAct/src/Routes/PrivateRoute.tsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { AuthContext } from "../Context/AuthContext";
-
-export default function PrivateRoute({ children }: any) {
-  const { user } = useContext(AuthContext);
-
-  return user ? children : <h1>403 - Unauthorized</h1>;
-}
